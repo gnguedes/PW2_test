a349289a89c9b41ea8d3a18d50a1e765e548e3c7
Guilherme  Costa <guilhermecosta@AirdeGuilherme.lan>
2020-06-04 12:28:51 +0100

    changes

--- Server/index.js
+++ Server/index.js
@@ -8,5 +8,5 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
 require("./routes/package.route")(app)
-//require("./routes/users.route")(app)
+require("./routes/users.route")(app)
 app.listen(port, () => console.log("listening on Port " + port))
--- Server/routes/users.route.js
+++ Server/routes/users.route.js
@@ -1,23 +1,7 @@
-const con = require("../connection")
-async function getUsers(req, res) {
-    const query = "select * from users;"
-    con.query(query, (err, results, fields) => {
-        if (err) {
-            return res.send(err)
-        }
-        res.send(results)
-    })
-}
+const express = require("express")
+const router = express.Router()
+const controller = require("../controllers/users.controller")
 
-async function addUser(req, res) {
-    const { name, email} = req.body
-    const query = `insert into encomenda (name, email, password) values ("${name}", "${email}", "${password}")`
-    con.query(query, (err, results, fields) => {
-        if (err) {
-            return res.send(err)
-        }
-        res.send(results)
-    })
-}
-
-module.exports = { getUsers, addUser}
+router.get("/", controller.getUsers)
+router.post("/", controller.addUser)
+module.exports = app => app.use("/users", router)
