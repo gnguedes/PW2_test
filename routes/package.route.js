54ecfe48d1cff9efd928280b7b3f108b1702eded
Guilherme  Costa <guilhermecosta@AirdeGuilherme.lan>
2020-06-04 10:45:16 +0100

    commit

--- routes/package.route.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const router = express.Router()
-const controller = require("../controllers/package.controller")
-
-router.get("/", controller.getPackage)
-router.post("/", controller.addPackage)
-router.delete("/:id", controller.deletePackage)
-module.exports = app => app.use("/package", router)
